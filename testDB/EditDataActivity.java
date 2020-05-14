0b1912d135eb7bc078ed42264051d061d32d4d4d
Mike Zeng <38231969+miek0@users.noreply.github.com>
2020-05-13 23:07:54 -0700

    Create EditDataActivity.java

---
+++ testDB/EditDataActivity.java
@@ -0,0 +1,80 @@
+package com.example.taskscheduler;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+/**
+ * Created by User on 2/28/2017.
+ */
+
+public class EditDataActivity extends AppCompatActivity {
+
+    private static final String TAG = "EditDataActivity";
+
+    private Button btnSave, btnDelete;
+    private EditText editable_item;
+
+    UserDatabase mUserDatabase;
+
+    private String selectedName;
+    private int selectedID;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.edit_data_layout);
+        btnSave = (Button) findViewById(R.id.btnSave);
+        btnDelete = (Button) findViewById(R.id.btnDelete);
+        editable_item = (EditText) findViewById(R.id.editable_item);
+        mUserDatabase = new UserDatabase(this);
+
+        //get the intent extra from the ListDataActivity
+        Intent receivedIntent = getIntent();
+
+        //now get the itemID we passed as an extra
+        selectedID = receivedIntent.getIntExtra("id", -1); //NOTE: -1 is just the default value
+
+        //now get the name we passed as an extra
+        selectedName = receivedIntent.getStringExtra("name");
+
+        //set the text to show the current selected name
+        editable_item.setText(selectedName);
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String item = editable_item.getText().toString();
+                if (!item.equals("")) {
+                    mUserDatabase.updateName(item, selectedID, selectedName);
+                } else {
+                    toastMessage("You must enter a name");
+                }
+            }
+        });
+
+        btnDelete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mUserDatabase.deleteName(selectedID, selectedName);
+                editable_item.setText("");
+                toastMessage("removed from database");
+            }
+        });
+
+    }
+
+    /**
+     * customizable toast
+     *
+     * @param message
+     */
+    private void toastMessage(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+}
