6cf0f09902be6655eab93d1f497fa3b8fbb94eed
Mike Zeng <38231969+miek0@users.noreply.github.com>
2020-05-15 00:36:07 -0700

    Update TaskDatabase.java

--- testDB/TaskDatabase.java
+++ testDB/TaskDatabase.java
@@ -51,7 +51,7 @@ public class TaskDatabase extends SQLiteOpenHelper {
 
         long result = db.insert(TABLE_NAME, null, contentValues);
 
-        //if date as inserted incorrectly it will return -1
+        //if data inserted incorrectly it will return -1
         if (result == -1) {
             return false;
         } else {
@@ -64,9 +64,10 @@ public class TaskDatabase extends SQLiteOpenHelper {
      *
      * @return
      */
-    public Cursor getData() {
+    public Cursor getDataOfUser(int userid) {
         SQLiteDatabase db = this.getWritableDatabase();
-        String query = "SELECT * FROM " + TABLE_NAME;
+        String query = "SELECT * FROM " + TABLE_NAME +
+                        "WHERE " + COL2 + " = " + userid;
         Cursor data = db.rawQuery(query, null);
         return data;
     }
@@ -96,40 +97,15 @@ public class TaskDatabase extends SQLiteOpenHelper {
     }
 
     /**
-     * Updates the name field
+     * Updates the a chosen field
      *
-     * @param newName
+     * @param newField
      * @param id
-     * @param oldName
+     * @param oldField
+     * @param userid
+     * @param col
      */
-    public void updateName(String newName, int id, String oldName) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        String query = "UPDATE " + TABLE_NAME + " SET " + COL2 +
-                " = '" + newName + "' WHERE " + COL1 + " = '" + id + "'" +
-                " AND " + COL2 + " = '" + oldName + "'";
-        Log.d(TAG, "updateName: query: " + query);
-        Log.d(TAG, "updateName: Setting name to " + newName);
-        db.execSQL(query);
-    }
-
-    /**
-     * Delete from database
-     *
-     * @param id
-     * @param name
-     */
-    public void deleteName(int id, String name) {
-        SQLiteDatabase db = this.getWritableDatabase();
-        String query = "DELETE FROM " + TABLE_NAME + " WHERE "
-                + COL1 + " = '" + id + "'" +
-                " AND " + COL2 + " = '" + name + "'";
-        Log.d(TAG, "deleteName: query: " + query);
-        Log.d(TAG, "deleteName: Deleting " + name + " from database.");
-        db.execSQL(query);
-    }
-
-
-    public void updateField(String newField, String oldField, int id, int userid, String col) {
+    public void updateTask(String newField, String oldField, int id, int userid, String col) {
         SQLiteDatabase db = this.getWritableDatabase();
         String query = "UPDATE " + TABLE_NAME + " SET " + col +
                 " = '" + newField + "' WHERE " + COL1 + " = '" + id + "'" +
@@ -139,4 +115,19 @@ public class TaskDatabase extends SQLiteOpenHelper {
         db.execSQL(query);
     }
 
+    /**
+     * Delete from database
+     *
+     * @param id
+     * @param name
+     */
+    public void deleteTask(int id, String name) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        String query = "DELETE FROM " + TABLE_NAME + " WHERE "
+                + COL1 + " = '" + id + "'" +
+                " AND " + COL3 + " = '" + name + "'";
+        Log.d(TAG, "deleteName: query: " + query);
+        Log.d(TAG, "deleteName: Deleting " + name + " from database.");
+        db.execSQL(query);
+    }
 }
